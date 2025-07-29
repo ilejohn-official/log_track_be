fc7a29491116bc5de78d499ddd4759947f2b87cf
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-07-29 22:15:52 +0100

    allow all hosts in settings.py

--- log_track_be/settings.py
+++ log_track_be/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-m5f$#q)y^jw!&qxr^xx2$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
